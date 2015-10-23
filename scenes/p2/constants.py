a4b48bc337f9f730a8883cbb1c9e732e506671f4
Scott Davidson <scottd297@yahoo.com.au>
2015-10-23 22:28:33 +0100

    Chair scene adjustments

--- scenes/p2/constants.py
+++ scenes/p2/constants.py
@@ -4,14 +4,19 @@ from director import add_input, add_output, PUD_UP
 # Call them something that makes sense for the setup
 
 print "create inputs"
-PIR_TRIGGER = add_input(21)
-CHAIR_TRIGGER = add_input(16)
-RESET_TRIGGER = add_input(12)
+DOOR_TRIGGER = add_input(17)		# Photo beam at feet level
+CHAIR_TRIGGER = add_input(27)		# Limit switch on the chair seat
+PROXIMITY_TRIGGER = add_input(17)	# PIR trigger welcome sound
+
+RESET_TRIGGER = add_input(22)		# External button outside room can reset before normal timeout
+
 
 print "create outputs"
-WELCOME_SIGN_RELAY = add_output(26)
-CHAIR_SPOT_RELAY = add_output(19)
-SIREN_RELAY = add_output(11)
-GREEN_LIGHT_RELAY = add_output(13)
-RED_LIGHT_RELAY = add_output(6)
-CHAIR_VIBRATE_RELAY = add_output(5)
+WELCOME_SIGN_RELAY = add_output(26)	# R1 - Flickering light for welcome sign
+CHAIR_SPOT_RELAY = add_output(19)	# R2 - Chair light on solid during idle. Flicker with the electric sound when chair is going
+SIREN_RELAY = add_output(11)		# R3 - Relay to turn on the rotating light
+GREEN_LIGHT_RELAY = add_output(13)	# R4 - Relay for yellow bulb
+RED_LIGHT_RELAY = add_output(6)		# R5 - Relay for red bulb
+CLAXON_RELAY = add_output(16)		# R6 - Relay for Claxon (needs pulses)
+CHAIR_VIBRATE_RELAY = add_output(5)	# R7 - Relay to actually turn the chair on
+AIR_BLAST_RELAY = add_output(20)		# R8 - Air solenoid (slight delay after vib starts then 2 sec pulse off 1 sec pulse off)
--- scenes/p2/scene_chair.py
+++ scenes/p2/scene_chair.py
@@ -1,6 +1,11 @@
 import director, random
 from constants import *
 
+OUTSIDE_CHANNEL = "left";
+INSIDE_CHANNEL = "right";
+
+switch_sound = director.load_sound("scenes/p2/audio/switch.ogg")
+ghost_story_sound = director.load_sound("scenes/p2/audio/ghost_story.ogg")
 
 def reset():
 	director.set_on(WELCOME_SIGN_RELAY)
@@ -8,33 +13,43 @@ def reset():
 	director.set_off(GREEN_LIGHT_RELAY)
 	director.set_off(RED_LIGHT_RELAY)
 
-	director.add_trigger(PIR_TRIGGER, enter, (), bouncetime=2)
+	director.add_trigger(PROXIMITY_TRIGGER, welcome, (), bouncetime=2)
+	director.add_trigger(DOOR_TRIGGER, enter, (), bouncetime=2)
 	director.add_trigger(CHAIR_TRIGGER, play, (), bouncetime=2)
 
+	director.play_sound(ghost_story_sound, loops=-1, channel=INSIDE_CHANNEL)
+
+def welcome():
+	director.play_sound("scenes/p2/audio/take_a_seat.ogg", channel=OUTSIDE_CHANNEL)
+
 def enter():
-	director.play_sound("scenes/p2/audio/take_a_seat.ogg")
+	# Stop the idle sounds
+	ghost_story_sound.stop()
+	director.play_sound("scenes/p2/audio/take_a_seat.ogg", channel=INSIDE_CHANNEL)
+
 	director.set_off(WELCOME_SIGN_RELAY, 2)
-	director.remove_trigger(PIR_TRIGGER)
+	director.remove_trigger(DOOR_TRIGGER)
 
 def play():
-	switch_sound = director.load_sound("scenes/p2/audio/switch.ogg")
+	# Stop the idle sounds
+	ghost_story_sound.stop()
 
 	# Siren
 	director.set_on(SIREN_RELAY, 0, 13)
-	director.play_sound("scenes/p2/audio/siren.ogg")
+	director.play_sound("scenes/p2/audio/siren.ogg", channel=INSIDE_CHANNEL)
 
 	# Green light and hum sound effect
-	director.play_sound(switch_sound, 5)
+	director.play_sound(switch_sound, 5, channel=INSIDE_CHANNEL)
 	director.set_on(GREEN_LIGHT_RELAY, 5.5, 7.5)
-	director.play_sound("scenes/p2/audio/switch_hum.ogg", 5, loops=-1, maxtime=4)
+	director.play_sound("scenes/p2/audio/switch_hum.ogg", 5, loops=-1, maxtime=4, channel=INSIDE_CHANNEL)
 	
 	# Red light
-	director.play_sound(switch_sound, 6)
+	director.play_sound(switch_sound, 6, channel=INSIDE_CHANNEL)
 	director.set_on(RED_LIGHT_RELAY, 6.5, 6.5)
 	
 	# fire the chair
 	director.set_on(CHAIR_VIBRATE_RELAY, 6.8, 5)
-	director.play_sound("scenes/p2/audio/electric.ogg", 6, maxtime=6.5)
+	director.play_sound("scenes/p2/audio/electric.ogg", 6, maxtime=6.5, channel=INSIDE_CHANNEL)
 
 	# Flicker chair light
 	director.set_off(CHAIR_SPOT_RELAY, 6.8)
@@ -51,8 +66,12 @@ def play():
 	director.set_on(CHAIR_SPOT_RELAY, 12.2, 0.3)
 	director.set_on(CHAIR_SPOT_RELAY, 12.8)
 
+	# Air blast
+	director.set_on(AIR_BLAST_RELAY, 6.9, 2)
+	director.set_on(AIR_BLAST_RELAY, 10, 1)
+
 	# End sound
-	director.play_sound(switch_sound, 12.5)
+	director.play_sound(switch_sound, 12.5, channel=INSIDE_CHANNEL)
 
 	# Reset
 	director.schedule(20, reset, ())
